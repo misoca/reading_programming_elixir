aac48c229b16351eebe840444d2b1cea83912596
Tomochika Hara <git@thara.jp>
2019-07-12 17:35:36 +0900

    :muscle: 第6章 練習問題

---
+++ chapter_6/thara/ModulesAndFunctions-6.exs
@@ -0,0 +1,24 @@
+defmodule Chop do
+  def guess(actual, range) do
+    (min..max) = range
+    center = div(max - min, 2) + min
+    IO.puts "Is it #{center}"
+    find(actual, range, center)
+  end
+
+  def find(actual, _, n) when actual == n do
+    IO.puts n
+  end
+
+  def find(actual, range, n) when actual < n do
+    (min.._) = range
+    guess(actual, min..n-1)
+  end
+
+  def find(actual, range, n) when actual > n do
+    (_..max) = range
+    guess(actual, n+1..max)
+  end
+end
+
+Chop.guess(273, 1..1000)
