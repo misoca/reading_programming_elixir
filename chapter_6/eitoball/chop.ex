acfbe39d96e5c261fdd966c92d5174aad0d0d54c
Eito Katagiri <eitoball@gmail.com>
2019-07-13 17:31:32 +0900

    この章までで出てきたパターンマッチングなどだけを使うように修正

--- chapter_6/eitoball/chop.ex
+++ chapter_6/eitoball/chop.ex
@@ -1,14 +1,14 @@
 defmodule Chop do
   def guess(actual, first..last) do
-    n = div(first + last, 2)
-    if n == actual do
-      IO.puts("Is it #{first}")
-    else
-      if n < actual do
-        guess(actual, n..last)
-      else
-        guess(actual, first..n)
-      end
-    end
+    guessed = div(first + last, 2)
+    IO.puts("Is it #{guessed}")
+    do_guess(actual, guessed, first, last)
   end
+
+  defp do_guess(actual, guessed, _, _) when actual == guessed,
+    do: guessed
+  defp do_guess(actual, guessed, first, _) when actual < guessed,
+    do: guess(actual, first..guessed)
+  defp do_guess(actual, guessed, _, last) when actual > guessed,
+    do: guess(actual, guessed..last)
 end
